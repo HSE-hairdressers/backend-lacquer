38fe1f498d5eb4804aefc5f8536c965c6b21175f
khadievedem <khadiev.edem@gmail.com>
2023-02-25 19:00:03 +0300

    add hairdresser structure

---
+++ src/server/hdresser.rs
@@ -0,0 +1,19 @@
+#[derive(Serialize)]
+pub struct Hairdresser {
+    pub name: String,
+    pub num: String,
+    pub addr: String,
+    pub company: String,
+}
+
+impl Hairdresser {
+    pub fn new(name: String, phone_number: String, address: String, company: String) -> Self {
+        Hairdresser {
+            name: (name),
+            num: (phone_number),
+            addr: (address),
+            company: (company),
+        }
+    }
+}
+
