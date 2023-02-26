88a7b7aad8cc260c9eac22c198661e45fb54b51b
khadievedem <khadiev.edem@gmail.com>
2023-02-26 22:01:44 +0300

    add struct for hairclassifier

--- src/server/response.rs
+++ src/server/response.rs
@@ -1,19 +1,34 @@
 use super::{hdresser::Hairdresser, photo::Photo};
-use serde::Serialize;
+use serde::{Serialize, Deserialize};
 
 #[derive(Serialize)]
 pub struct UserImageResponse {
     pub hairdresser: Hairdresser,
     pub images: Vec<Photo>,
     result: String,
+    hairstyle: String,
 }
 
 impl UserImageResponse {
-    pub fn new(h_name: Hairdresser, images: Vec<Photo>) -> Self {
+    pub fn new(h_name: Hairdresser, images: Vec<Photo>, hairstyle: &str) -> Self {
         UserImageResponse {
             hairdresser: (h_name),
             images: (images),
             result: ("Ok".to_string()),
+            hairstyle: (hairstyle.to_string()),
         }
     }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ImageSize {
+    width: u16,
+    height: u16,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct HairClassifierResponse {
+    pub message: String,
+    pub size: ImageSize,
+    pub result: String,
 }
