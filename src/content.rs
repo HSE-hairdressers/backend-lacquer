e6666c6cf0ecd2456c15edfd08901d7979af2c39
khadievedem <khadiev.edem@gmail.com>
2023-03-31 23:40:15 +0300

    remove temp impl, add new interface

--- src/content.rs
+++ src/content.rs
@@ -1,9 +1,9 @@
 use crate::server::{
-    hdresser::Hairdresser,
     photo::Photo,
     response::{DataResponse, HairClassifierResponse, UserImageResponse},
     sysinfo::SystemInfo,
 };
+use crate::repository::db;
 use actix_multipart::Multipart;
 use actix_web::{
     get, http::header::ContentType, post, web, Error, HttpResponse, Responder, Result,
@@ -12,16 +12,6 @@ use futures_util::StreamExt as _;
 use uuid::Uuid;
 
 use std::io::Write;
-
-#[get("/hello")]
-pub async fn hello() -> impl Responder {
-    "Hello World!".to_string()
-}
-
-#[get("/hello/{name}")]
-pub async fn greet(name: web::Path<String>) -> impl Responder {
-    format!("Hello {name} !")
-}
 
 #[get("/system/info")]
 pub async fn sys_info() -> impl Responder {
@@ -37,6 +27,7 @@ pub async fn sys_info() -> impl Responder {
     }
     return web::Json(info);
 }
+
 #[post("/img")]
 pub async fn img(mut payload: Multipart) -> Result<HttpResponse, Error> {
     let mut filename = String::new();
@@ -55,10 +46,8 @@ pub async fn img(mut payload: Multipart) -> Result<HttpResponse, Error> {
         let _ = std::fs::create_dir("./tmp");
         let filepath = format!("./tmp/{filename}");
 
-        // File::create is blocking operation, use threadpool
         let mut file = web::block(|| std::fs::File::create(filepath)).await??;
 
-        // Field in turn is stream of *Bytes* object
         while let Some(chunk) = field.next().await {
             file = web::block(move || {
                 file.write_all(&chunk.ok().unwrap_or_default())
@@ -88,38 +77,63 @@ pub async fn img(mut payload: Multipart) -> Result<HttpResponse, Error> {
 
     println!("[INFO] Got hairstyle {:#?}", hairstyle);
 
-    let f_path = format!("./onlyfaces/{}/", hairstyle.result);
-    println!("[INFO] Try to open dir {:#?}", f_path);
-    let paths = std::fs::read_dir(f_path.to_string()).unwrap();
-
-    println!("[INFO] Create hairdresser");
-    let hairdresser = Hairdresser::new(
-        "Khadiev Edem".to_string(),
-        "+7 999 123 45 67".to_string(),
-        "NN, Test st., 100100".to_string(),
-        "HSE-hairdressers".to_string(),
-    );
-
-    let mut photos: Vec<Photo> = Vec::new();
-    let mut i = 0;
-    println!("[INFO] Add all photos to a photos");
-    for path in paths.into_iter() {
-        i += 1;
-        let photo = Photo::new(format!("{i} photo"), format!("http://79.137.206.63:8000/{}/{}", hairstyle.result.replace(" ", "_"), path.unwrap().file_name().into_string().unwrap()));
-        photos.push(photo);
+    if let Some(hstyle) = hairstyle.get_result() {
+        let mut response = UserImageResponse::new("Ok");
+        let hdressers = db::get_hairdressers(&hstyle);
+        for hdresser in hdressers {
+            let img_urls = db::get_picture_links(hdresser.get_id(), &hstyle);
+            let data_res = DataResponse::new(hdresser, Photo::from_vec(&img_urls));
+            response.add_data(data_res);
+        }
+        Ok(HttpResponse::Ok()
+            .content_type(ContentType::json())
+            .json(response)
+            .into())
+    } else {
+        let response = UserImageResponse::new("Error");
+        Ok(HttpResponse::Ok().content_type(ContentType::json()).json(response).into())
     }
 
-    let data_res = DataResponse::new(hairdresser, photos);
-    
-    let result = match hairstyle.result.as_str() {
-        "0" => "Error",
-        _ => "Ok",
-    };
-    let mut response = UserImageResponse::new(result);
-    response.add_data(data_res);
 
-    Ok(HttpResponse::Ok()
-        .content_type(ContentType::json())
-        .json(response)
-        .into())
+
+
+
+    // let f_path = format!("./onlyfaces/{}/", hairstyle.result);
+    // println!("[INFO] Try to open dir {:#?}", f_path);
+    // let paths = std::fs::read_dir(f_path.to_string()).unwrap();
+
+    // println!("[INFO] Create hairdresser");
+    // let hairdresser = Hairdresser::new(
+    //     ""
+    //     "Khadiev Edem".to_string(),
+    //     "+7 999 123 45 67".to_string(),
+    //     "NN, Test st., 100100".to_string(),
+    //     "HSE-hairdressers".to_string(),
+    // );
+
+    // let mut photos: Vec<Photo> = Vec::new();
+    // let mut i = 0;
+    // println!("[INFO] Add all photos to a photos");
+    // for path in paths.into_iter() {
+    //     i += 1;
+    //     let photo = Photo::new(
+    //         format!("{i} photo"),
+    //         format!(
+    //             "http://79.137.206.63:8000/{}/{}",
+    //             hairstyle.result.replace(" ", "_"),
+    //             path.unwrap().file_name().into_string().unwrap()
+    //         ),
+    //     );
+    //     photos.push(photo);
+    // }
+
+    // let data_res = DataResponse::new(hairdresser, photos);
+
+    // let result = match hairstyle.result.as_str() {
+    //     "0" => "Error",
+    //     _ => "Ok",
+    // };
+    // let mut response = UserImageResponse::new(result);
+    // response.add_data(data_res);
+
 }
