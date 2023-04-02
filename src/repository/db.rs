c8ac6050fbb8cc563766734b0051dd981d0cae5b
khadievedem <khadiev.edem@gmail.com>
2023-04-02 21:18:54 +0300

    remove useless init_db and fix bugs

--- src/repository/db.rs
+++ src/repository/db.rs
@@ -32,51 +32,6 @@ pub fn create_hairdresser(hd_name: &str, hd_email: &str) {
     connection.execute(query).unwrap();
 }
 
-pub fn init_db() {
-    if !std::path::Path::new(&DB_PATH).exists() {
-        let connection = sqlite::open(DB_PATH).unwrap();
-        let query = DatabaseQuery::init_db();
-        connection.execute(query).unwrap();
-
-        for style in HAIRSTYLES {
-            let ins_query = format!("INSERT INTO hairstyles VALUES ('{style}');");
-            connection.execute(&ins_query).unwrap();
-        }
-
-        let baby_dressers: Vec<_> = vec![
-            (
-                DatabaseQuery::get_hdresser_id("khadiev.edem@gmail.com"),
-                "Khadiev Edem",
-            ),
-            (
-                DatabaseQuery::get_hdresser_id("max.md8@gmail.com"),
-                "Dudarev Maxim",
-            ),
-            (
-                DatabaseQuery::get_hdresser_id("ageev.maxim2003@gmail.com"),
-                "Ageev Maxim",
-            ),
-        ];
-
-        for query in baby_dressers {
-            let mut statement = connection.prepare(&query.0 .0).unwrap();
-            let mut baby_dr = String::new();
-            while let Ok(sqlite::State::Row) = statement.next() {
-                baby_dr = statement.read::<String, _>(query.0 .1.as_str()).unwrap();
-            }
-            for style in HAIRSTYLES {
-                let ins_query = format!(
-                    "
-                    INSERT INTO style_to_dresser VALUES ({baby_dr}, '{}', '{style}');
-                    ",
-                    query.1
-                );
-                connection.execute(&ins_query).unwrap();
-            }
-        }
-    }
-}
-
 pub fn get_picture_links(hdresser_id: i64, hstyle: &str) -> Vec<String> {
     let connection = sqlite::open(DB_PATH).unwrap();
     let query = DatabaseQuery::get_picture_urls(hdresser_id, hstyle);
@@ -86,7 +41,7 @@ pub fn get_picture_links(hdresser_id: i64, hstyle: &str) -> Vec<String> {
 
     let mut pictures: Vec<String> = vec![];
     while let Ok(sqlite::State::Row) = statement.next() {
-        pictures.push(statement.read::<String, _>(query.1.as_str()).unwrap());
+        pictures.push(format!("https://79.137.206.63:8000/{}", statement.read::<String, _>(query.1.as_str()).unwrap()));
     }
     pictures
 }
