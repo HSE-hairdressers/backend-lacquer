22024392907091c9e6e9d6c35aad5200391662bf
khadievedem <khadiev.edem@gmail.com>
2023-04-21 13:04:51 +0300

    add setters

--- src/server/hdresser.rs
+++ src/server/hdresser.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Debug)]
+#[derive(Serialize, Debug, Default)]
 pub struct Hairdresser {
     id: i64,
     email: String,
@@ -29,6 +29,29 @@ impl Hairdresser {
             addr: (address),
             company: (company),
         }
+    }
+
+    pub fn with_id(id: i64) -> Self {
+        Self {
+            id: (id),
+            ..Self::default()
+        }
+    }
+
+    pub fn set_email(&mut self, email: &str) {
+        self.email = email.to_string();
+    }
+    pub fn set_name(&mut self, name: &str) {
+        self.name = name.to_string();
+    }
+    pub fn set_num(&mut self, num: &str) {
+        self.num = num.to_string();
+    }
+    pub fn set_address(&mut self, addr: &str) {
+        self.address = addr.to_string();
+    }
+    pub fn set_company(&mut self, company: &str) {
+        self.company = company.to_string();
     }
 
     pub fn get_id(&self) -> i64 {
