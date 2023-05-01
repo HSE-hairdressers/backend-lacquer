51b2d3fd7a57edc406059d7e2e967021f7960e5d
khadievedem <khadiev.edem@gmail.com>
2023-05-01 11:21:43 +0300

    remove "name" field from photo struct

--- src/server/photo.rs
+++ src/server/photo.rs
@@ -2,14 +2,12 @@ use serde::Serialize;
 
 #[derive(Serialize)]
 pub struct Photo {
-    pub name: String,
     pub img_path: String,
 }
 
 impl Photo {
-    pub fn new(name: String, path: String) -> Self {
+    pub fn new(path: String) -> Self {
         Photo {
-            name: (name),
             img_path: (path),
         }
     }
@@ -17,7 +15,6 @@ impl Photo {
         let mut ans: Vec<Self> = Vec::with_capacity(paths.capacity());
         for path in paths {
             ans.push(Self::new(
-                path.clone().split("/").last().unwrap().to_string(),
                 path,
             ));
         }
